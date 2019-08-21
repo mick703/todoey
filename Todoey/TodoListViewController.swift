bfe460b1de55e918bba3ccb6e7a31aabfa6d96ef
Ming <ming.yan@thedailyedited.com>
2019-08-21 13:12:06 +1000

    Added a table view controller with a few initial styling updates

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Ming Yan on 21/8/19.
+//  Copyright © 2019 Ming Yan. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    let itemArray = ["One", "Two", "Three"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath);
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+
+}
+
